a6b68fd07dea2c5b86bd21f8913968bcde8a373a
dnmcky <dnmckywork@gmail.com>
2019-12-02 22:15:50 +0000

    wee refactor

--- 2019/01/solve.js
+++ 2019/01/solve.js
@@ -1,14 +1,16 @@
-function solve(masses, part) {
+function solve(input, part) {
+    const masses = input.map(x => Number(x))
     return masses.reduce(reducer, 0);
 }
  
 const reducer = (accumulator, mass) => {
-    const currentTotalFuel = parseInt(accumulator);
-    const currentMass = parseInt(mass);
-    const journeyFuel = Math.floor((currentMass/3)) - 2;
-    return currentTotalFuel + journeyFuel;
+    const journeyFuel = massToFuel(mass);
+    return accumulator + journeyFuel;
 };
- 
+
+const massToFuel = (mass) => {
+    return Math.floor((mass/3)) - 2;
+}
  const expected = part => part === 1 ? 3349352 : -1;
  
- module.exports = { solve,expected };
+ module.exports = { solve, expected };
