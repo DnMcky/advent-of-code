87f2e3ad67d51c6a392682b2879a0a94ff887e69
dnmcky <dnmckywork@gmail.com>
2019-12-03 08:34:37 +0000

    make part 2 solvable in solution and solve.

--- 2019/01/solve.js
+++ 2019/01/solve.js
@@ -1,16 +1,23 @@
 function solve(input, part) {
     const masses = input.map(x => Number(x))
-    return masses.reduce(reducer, 0);
+    return masses.reduce(reducer(part), 0);
 }
  
-const reducer = (accumulator, mass) => {
-    const journeyFuel = massToFuel(mass);
+const reducer = (part) => (accumulator, mass) => {
+    const journeyFuel = part === 1 ? massToFuel(mass) : fuelMassAdjusted(mass);
     return accumulator + journeyFuel;
 };
+
+const fuelMassAdjusted = (n, acc = 0) => {
+    const x = massToFuel(n);
+    if (x <= 0) return acc; 
+    return fuelMassAdjusted(x, acc + x);
+}
 
 const massToFuel = (mass) => {
     return Math.floor((mass/3)) - 2;
 }
- const expected = part => part === 1 ? 3349352 : -1;
+
+const expected = part => part === 1 ? 3349352 : 5021154;
  
  module.exports = { solve, expected };
