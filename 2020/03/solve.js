7df103868a4a796d3fd55599e9bf07d96ab85dd8
dnmcky <dnmckywork@gmail.com>
2020-12-06 16:45:21 +0000

    day 3 solution

--- 2020/03/solve.js
+++ 2020/03/solve.js
@@ -1,7 +1,32 @@
 const solve = (input, part) => {
-    return part === 0 ? 1 : 2;
+    const movePatterns = [
+        { x: 1, y: 1},
+        { x: 3, y: 1},
+        { x: 5, y: 1},
+        { x: 7, y: 1},
+        { x: 1, y: 2},
+    ];
+
+    const results = movePatterns.map(({x, y}) => calculateTreesHit(input, x, y))
+
+    return part === 1 ? results[1] : results.reduce((acc, currentValue) => acc * currentValue);
 }
 
-const expected = part => part === 1 ? 980499 : 200637446;
+const calculateTreesHit = (input, moveX, moveY) => {
+    const MAP_WIDTH = input[0].length;
+    let xCoord = 0;
+    let treeCount = 0;
+    for (yCoord = moveY; yCoord < input.length; yCoord+=moveY) {
+        const row = input[yCoord].split('');
+        xCoord += moveX
+        if (xCoord > MAP_WIDTH-1) xCoord -= MAP_WIDTH;
+        if (row[xCoord] === '#') {
+            treeCount++;
+        }
+    }
+    return treeCount;
+}
 
-module.exports = { solve, expected };
+const expected = part => part === 1 ? 282 : 958815792;
+
+module.exports = { solve, expected, calculateTreesHit };
--- 2020/03/solve.spec.js
+++ 2020/03/solve.spec.js
@@ -1,9 +1,42 @@
 const { 
     solve, 
+    calculateTreesHit,
 } = require('./solve');
   
   test('solve', () => {
-    expect(solve(input, 1)).toBe(2);
-    expect(solve(input, 2)).toBe(1);
+    const input = [
+      "..##.......",// 0,0
+      "#...#...#..",// 1,3
+      ".#....#..#.",// 2,6
+      "..#.#...#.#",// 3,9
+      ".#...##..#.",// 4,12(0?)
+      "..#.##.....",// 5,15
+      ".#.#.#....#",// 6,18
+      ".#........#",// 7,21
+      "#.##...#...",// 8,24
+      "#...##....#",// 9,27
+      ".#..#...#.#"]// 10,30
+    expect(solve(input, 1)).toBe(7);
+    expect(solve(input, 2)).toBe(336);
+  });
+
+  test('solve', () => {
+    const input = [
+      "..##.......",// 0,0
+      "#...#...#..",// 1,3
+      ".#....#..#.",// 2,6
+      "..#.#...#.#",// 3,9
+      ".#...##..#.",// 4,12(0?)
+      "..#.##.....",// 5,15
+      ".#.#.#....#",// 6,18
+      ".#........#",// 7,21
+      "#.##...#...",// 8,24
+      "#...##....#",// 9,27
+      ".#..#...#.#"]// 10,30
+    expect(calculateTreesHit(input, 3, 1)).toBe(7);
+    expect(calculateTreesHit(input, 1, 1)).toBe(2);
+    expect(calculateTreesHit(input, 5, 1)).toBe(3);
+    expect(calculateTreesHit(input, 7, 1)).toBe(4);
+    expect(calculateTreesHit(input, 1, 2)).toBe(2);
   });
   
