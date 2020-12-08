2f9d57c2b2820edc273ecf444656169247213b14
dnmcky <dnmckywork@gmail.com>
2020-12-08 13:20:30 +0000

    day 5

--- 2020/05/solve.js
+++ 2020/05/solve.js
@@ -1,9 +1,59 @@
-var _ = require('lodash');
-
 const solve = (input, part) => {
-    return part === 1 ? 1 : 2;
+    
+    const coords = input.map((boardingPass) => {
+        return getSeatsRowAndColumn(boardingPass);
+    });
+
+    const seatIds = coords.map(x => {
+        const [row, column] = x;
+        return row * 8 + column;
+    });
+
+    const sortedSeatIds = seatIds.sort((a,b) => a - b);
+    const seatIdBeforeMine = sortedSeatIds.find((element, index, array) => {
+        return array[index+1] - element > 1;
+    })
+
+    const biggestId = Math.max(...seatIds);
+    return part === 1 ? biggestId : seatIdBeforeMine + 1;
 }
 
-const expected = part => part === 1 ? 2 : 4;
+const getSeatsRowAndColumn = (binaryMap) => {
+    let rowCodes = [];
+    for (var i = 0; i <= 127; i++) {
+        rowCodes.push(i);
+    }
+    let columnCodes = [];
+    for (var i = 0; i <= 7; i++) {
+        columnCodes.push(i);
+    }
 
-module.exports = { solve, expected };
+    const binaryList = binaryMap.split('');
+    const rowMap = binaryList.slice(0,7);
+    const columnMap = binaryList.slice(7, 10);
+
+    const row = rowMap.reduce(rowReducer, rowCodes);
+    const column = columnMap.reduce(columnReducer, columnCodes);
+    return [row[0], column[0]]
+}
+
+// a closure might be a good way to reduce this duplication here 
+const rowReducer = (acc, currentValue) => {
+    const half_length = Math.ceil(acc.length / 2);    
+    const leftSide = acc.slice(0,half_length);
+    const rightSide = acc.slice(half_length, acc.length);
+    
+    return currentValue === "F" ? leftSide : rightSide;
+}
+
+const columnReducer = (acc, currentValue) => {
+    const half_length = Math.ceil(acc.length / 2);    
+    const leftSide = acc.slice(0,half_length);
+    const rightSide = acc.slice(half_length, acc.length);
+    
+    return currentValue === "L" ? leftSide : rightSide;
+}
+
+const expected = part => part === 1 ? 871 : 640;
+
+module.exports = { solve, expected, getSeatsRowAndColumn };
--- 2020/05/solve.spec.js
+++ 2020/05/solve.spec.js
@@ -1,7 +1,18 @@
-const { solve } = require('./solve');
+const { solve, getSeatsRowAndColumn } = require('./solve');
   
 test('solve', () => {
-  expect(solve(input, 1)).toBe(2);
-  expect(solve(input, 2)).toBe(4);
+  const input = [
+    'BFFFBBFRRR',
+  'FFFBBBFRRR',
+  'BBFFBBFRLL'
+  ]
+  expect(solve(input, 1)).toBe(820);
+  expect(solve(input, 2)).toBe(640);
+});
+
+test('getSeatsRowAndColumn', () => {
+  expect(getSeatsRowAndColumn("BFFFBBFRRR")).toStrictEqual([70,7]);
+  expect(getSeatsRowAndColumn("FFFBBBFRRR")).toStrictEqual([14,7]);
+  expect(getSeatsRowAndColumn("BBFFBBFRLL")).toStrictEqual([102,4]);
 });
 
